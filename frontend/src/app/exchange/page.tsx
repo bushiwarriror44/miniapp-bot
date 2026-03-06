19848a92e0ddb8cc3632150f91820c8b0bd3c673
bushiwarriror <bushiwarriror44@proton.me>
2026-03-06 20:51:25 +0200

    update v1.12

--- frontend/src/app/exchange/page.tsx
+++ frontend/src/app/exchange/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useState, useEffect } from "react";
+import { Suspense, useState, useEffect, useCallback } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faXmark } from "@fortawesome/free-solid-svg-icons";
